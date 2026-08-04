f934331aba941088ccfaf2b9e5ff20d6e5848ef3
illuminatedminds <michael.mathews@illuminatedmindseg.com>
2026-08-03 22:41:40 -0700

    Add starter JS

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// Portfolio interactivity
+console.log("Portfolio loaded!");
+document.querySelectorAll("nav a").forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute("href")).scrollIntoView({ behavior: "smooth" });
+  });
+});
